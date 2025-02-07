212a4fff8ce24a5fd89775ff607ee2d934bbf4e4
saurabhsiddhartha <saurabhsiddhartha82@gmail.com>
2025-02-07 13:28:55 +0530

    frontend url

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,32 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const connectDB = require("./db/dbconnection"); 
+const connectDB = require("./db/dbconnection");
 const Note = require("./route/Note");
 require("dotenv").config();
-const Auth =require('./route/Auth') 
-const app = express();  
+const Auth = require("./route/Auth");
+
+const app = express();
 connectDB(); 
-app.use(express.json()); 
-app.use(cors()); 
+app.use(express.json());  
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL.replace(/\/$/, ""), // Remove trailing slash
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true, // Allow cookies if needed
+    credentials: true,
     allowedHeaders: "Content-Type, Authorization",
   })
 );
 
+app.options("*", cors());
+
 app.use(bodyParser.json());
-app.get('/',(req,res)=>{
-    res.send("Hello from backend")
-})
+
+app.get("/", (req, res) => {
+  res.send("Hello from backend");
+});
+
 app.use("/api/note", Note);
 app.use("/api/user", Auth);
 
